9f1b55cdc0bfac0766f80810220d3f62219b820a
GloriaSekyere <gloriasekyere15@gmail.com>
2022-03-13 11:31:52 +0000

    compare card choices using useEffect. In each instance, call resetTurn function whether cards match or not

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 import SingleCard from './components/SingleCard';
 
@@ -18,6 +18,16 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
 
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        resetTurn()
+      } else {
+        resetTurn()
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
@@ -33,7 +43,7 @@ function App() {
 
   const resetTurn = () => {
     setChoiceOne(null)
-    setChoiceOne(null)
+    setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
   }
   
